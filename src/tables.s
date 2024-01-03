085d3cf7577f2dda0a8c0c6e69357ce4d1119a52
Lukas Krickl <lukas@krickl.dev>
2024-01-03 21:22:05 +0100

    WIP: test tables for items

--- src/tables.s
+++ src/tables.s
@@ -1,4 +1,23 @@
 ; this file contains various tables 
 
 ; item table 
+#macro itmdef 
 
+.db $1 ; type 
+.db $2 ; quality 
+
+.db $3 & 0xFF ; usefn 
+.db ($3 >> 8) & 0xFF ; usefn 
+
+.db $4 ; hp 
+.db $5 ; mp
+.db $6 ; str
+.db $7 ; int 
+.db $8 ; lck
+.db $9 ; dmg 
+.db $10 ; tile 
+.str $11 ; name 
+
+#endmacro 
+
+itmdef 1, 2, 3, 4, 5, 6, 7, 8, 9, "test", 12
